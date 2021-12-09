84c08fe724aa1839710e431201f5edb311af1762
Константин Харченко <kharshenko.kos@gmail.com>
2021-12-09 19:26:27 +0300

    vue install

---
+++ frontend/src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page edited</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data(){
+
+  },
+  getAge(){
+
+  }
+}
+</script>
